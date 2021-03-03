db7ffdc4406ae42066b5562995ca38c35a906af7
Daniel Torres Sandi <daniel.torres@owasp.org>
2021-03-03 14:49:56 +0000

    Create web-tools.sh

---
+++ web-tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function print_ascii_art {
+cat << "EOF"
+                 _                                     _         
+                ( )                                  _( )_       
+ _   _   _   __ | |_      ___ _   _   ___ _   _ _ __(_)  _)_   _ 
+( ) ( ) ( )/ __ \  _ \  /  __) ) ( )/ ___) ) ( )  __) | | ( ) ( )
+| \_/ \_/ |  ___/ |_) ) \__  \ (_) | (___| (_) | |  | | |_| (_) |
+ \__/\___/ \____)_ __/  (____/\___/ \____)\___/(_)  (_)\__)\__  |
+                                                          ( )_| |
+                                                           \___/ 
+
+EOF
+}
+
+
+print_ascii_art
+
+RED="\033[01;31m"      # Issues/Errors
+GREEN="\033[01;32m"    # Success
+YELLOW="\033[01;33m"   # Warnings/Information
+BLUE="\033[01;34m"     # Heading
+BOLD="\033[01;01m"     # Highlight
+RESET="\033[00m"       # Normal
+
+echo -e "${GREEN} [+] Instalando herramientas disponibles en repositorio ${RESET}" 
+sudo apt-get update
