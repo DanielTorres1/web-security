dcb2f383e796e547b50121470deb9bc615f035dc
daniel <daniel.torres@owasp.org>
2021-03-07 22:27:04 -0400

    update

--- web-tools.sh
+++ web-tools.sh
@@ -26,3 +26,12 @@ RESET="\033[00m"       # Normal
 
 echo -e "${GREEN} [+] Instalando herramientas disponibles en repositorio ${RESET}" 
 sudo apt-get update
+sudo apt install npm
+
+echo -e "${GREEN} [+] Instalando herramientas NPM ${RESET}" 
+sudo npm install -g snyk
+
+
+
+cd /home/kali/web-security/5-OWASP-TOP-10/A9/backend-master
+npm install
