447264bbe1a08ff197ca6e7262486277e8e89460
daniel <daniel.torres@owasp.org>
2021-03-15 08:24:41 -0400

    update

--- web-tools.sh
+++ web-tools.sh
@@ -26,7 +26,31 @@ RESET="\033[00m"       # Normal
 
 echo -e "${GREEN} [+] Instalando herramientas disponibles en repositorio ${RESET}" 
 sudo apt-get update
-sudo apt-get install geany 
-sudo wget https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2/jython-standalone-2.7.2.jar -o /opt/jython-standalone-2.7.2.jar
-sudo cp -r payloads /opt
+sudo apt-get install -y geany python3-pip synaptic golang-go libreoffice-calc
 
+
+echo -e "${GREEN} [+] Instalando WEBHACKS ${RESET}" 
+git clone https://github.com/DanielTorres1/Webhacks
+cd Webhacks
+sudo bash instalar.sh
+cd ..
+
+echo -e "${GREEN} [+] Instalando webintruder ${RESET}" 
+git clone https://github.com/DanielTorres1/webintruder
+cd webintruder
+sudo bash instalar.sh
+cd ..
+
+echo -e "${GREEN} [+] Instalando Dalfox ${RESET}" 
+git clone https://github.com/hahwul/dalfox
+cd dalfox
+sudo go install
+sudo go build
+sudo mv dalfox /usr/bin
+cd ..
+
+
+
+#sudo wget https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.2/jython-standalone-2.7.2.jar -o /opt/jython-standalone-2.7.2.jar
+
+#sudo apt-get -y install bc nbtscan nfs-common snmp finger sqlite3 sqlitebrowser python-pip nmap masscan onesixtyone whatweb libssl-dev ike-scan postgresql-client elinks smbclient bc libcurl4-openssl-dev xterm ipmitool lbd exiftool libpq-dev libpcap-dev tshark p7zip-full default-mysql-client python3-pip libssl-dev swig python3-dev gcc libcrypt-ssleay-perl metasploit-framework patator hydra enum4linux wpscan dnsutils python3-setuptools libreoffice-calc
